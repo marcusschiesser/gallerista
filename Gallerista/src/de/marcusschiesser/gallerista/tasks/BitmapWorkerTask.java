9e9afea7120e619f7a81a519c7991920d98042e2
marcus <mail@marcusschiesser.de>
2013-03-02 21:46:33 +0800

    changed app to 1000 wallpapers by adding set wallpaper functionality

--- Gallerista/src/de/marcusschiesser/gallerista/tasks/BitmapWorkerTask.java
+++ Gallerista/src/de/marcusschiesser/gallerista/tasks/BitmapWorkerTask.java
@@ -18,25 +18,26 @@ import de.marcusschiesser.gallerista.utils.ImageUtils;
 
 /**
  * AsyncTask that loads the Bitmap of an image which is referenced by its URL.
- * After loading the Bitmap is stored in the provided ImageView.
- * If multiple tasks are started for one ImageView, the last one can update the
- * ImageView. That way it supports ImageViews stored in a GridView which a 
- * shared for multiple positions.
- * While loading the Task shows a AnimationDrawable in the ImageView.
- * Previously loaded images are not loaded twice but retrieved from a cache.
- * To initiate a new task and thereby use this class use the loadBitmap method.
+ * After loading the Bitmap is stored in the provided ImageView. If multiple
+ * tasks are started for one ImageView, the last one can update the ImageView.
+ * That way it supports ImageViews stored in a GridView which a shared for
+ * multiple positions. While loading the Task shows a AnimationDrawable in the
+ * ImageView. Previously loaded images are not loaded twice but retrieved from a
+ * cache. To initiate a new task and thereby use this class use the loadBitmap
+ * method.
  * 
  * @author Marcus
  */
 public class BitmapWorkerTask extends AsyncTask<URL, Void, Bitmap> {
 
-	private final WeakReference<ImageView> mImageViewReference;
+	private final LoadingCallback mCallback;
 	private URL mUrl;
 	private IOException mIoException;
 
-	private BitmapWorkerTask(ImageView imageView) {
-		// Use a WeakReference to ensure the ImageView can be garbage collected
-		mImageViewReference = new WeakReference<ImageView>(imageView);
+	private BitmapWorkerTask(final LoadingCallback callback) {
+		if (callback == null)
+			throw new IllegalArgumentException("LoadingCallback must be set.");
+		mCallback = callback;
 		mIoException = null;
 	}
 
@@ -46,9 +47,9 @@ public class BitmapWorkerTask extends AsyncTask<URL, Void, Bitmap> {
 		mUrl = param[0];
 		try {
 			Bitmap bitmap = BitmapCacheUtils.getBitmapFromDiskCache(mUrl);
-			if(bitmap==null) {
+			if (bitmap == null) {
 				bitmap = ImageUtils.readBitmapFromURL(mUrl);
-				if(bitmap!=null) {
+				if (bitmap != null) {
 					BitmapCacheUtils.addBitmapToCache(mUrl, bitmap);
 				}
 			}
@@ -62,45 +63,47 @@ public class BitmapWorkerTask extends AsyncTask<URL, Void, Bitmap> {
 	// Once complete, see if ImageView is still around and set bitmap.
 	@Override
 	protected void onPostExecute(Bitmap bitmap) {
-		if(mIoException!=null) {
-			ExceptionUtils.handleException(mIoException, R.string.error_loading_image);
+		if (mIoException != null) {
+			ExceptionUtils.handleException(mIoException,
+					R.string.error_loading_image);
 		}
 		if (isCancelled()) {
 			bitmap = null;
 		}
 
-		if (mImageViewReference != null && bitmap != null) {
-			final ImageView imageView = mImageViewReference.get();
-			final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
-			if (this == bitmapWorkerTask && imageView != null) {
-				imageView.setImageBitmap(bitmap);
+		if (bitmap != null) {
+			final BitmapWorkerTask bitmapWorkerTask = mCallback
+					.getBitmapWorkerTask();
+			if (this == bitmapWorkerTask) {
+				mCallback.setImageBitmap(bitmap, mIoException == null);
 			}
 		}
 	}
 
 	/**
 	 * Loads an image from the provided url to the provided ImageView.
+	 * 
 	 * @param ctx
 	 * @param url
 	 * @param imageView
 	 */
-	public static void loadBitmap(Context ctx, URL url, ImageView imageView) {
+	public static void loadBitmap(Context ctx, URL url,
+			final LoadingCallback callback) {
 		final Bitmap bitmap = BitmapCacheUtils.getBitmapFromMemCache(url);
 		if (bitmap != null) {
-			imageView.setImageBitmap(bitmap);
+			callback.setImageBitmap(bitmap, true);
 		} else {
-			if (cancelPotentialWork(url, imageView)) {
-				final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
-				final AsyncDrawable asyncDrawable = new AsyncDrawable(ctx, task);
-				imageView.setImageDrawable(asyncDrawable);
-				asyncDrawable.start();
+			if (cancelPotentialWork(url, callback)) {
+				BitmapWorkerTask task = new BitmapWorkerTask(callback);
+				callback.setBitmapWorkerTask(task);
 				task.execute(url);
 			}
 		}
 	}
 
-	private static boolean cancelPotentialWork(URL url, ImageView imageView) {
-		final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+	private static boolean cancelPotentialWork(URL url, LoadingCallback callback) {
+		final BitmapWorkerTask bitmapWorkerTask = callback
+				.getBitmapWorkerTask();
 
 		if (bitmapWorkerTask != null) {
 			final URL bitmapUrl = bitmapWorkerTask.mUrl;
@@ -117,15 +120,73 @@ public class BitmapWorkerTask extends AsyncTask<URL, Void, Bitmap> {
 		return true;
 	}
 
-	private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
-		if (imageView != null) {
-			final Drawable drawable = imageView.getDrawable();
-			if (drawable instanceof AsyncDrawable) {
-				final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
-				return asyncDrawable.getBitmapWorkerTask();
+	public interface LoadingCallback {
+		void setImageBitmap(Bitmap bitmap, boolean success);
+		void setBitmapWorkerTask(BitmapWorkerTask task);
+		BitmapWorkerTask getBitmapWorkerTask();
+	}
+
+	public static void loadBitmap(final Context ctx, final URL url,
+			final ImageView imageView) {
+		loadBitmap(ctx, url, new ImageViewLoadingCallback(ctx, imageView));
+	}
+
+	public static abstract class DefaultLoadingCallback implements
+			LoadingCallback {
+		private BitmapWorkerTask mBitmapWorkerTaskReference = null;
+
+		@Override
+		public void setBitmapWorkerTask(BitmapWorkerTask task) {
+			mBitmapWorkerTaskReference = task;
+		}
+
+		@Override
+		public BitmapWorkerTask getBitmapWorkerTask() {
+			return mBitmapWorkerTaskReference;
+		}
+
+	}
+
+	public static class ImageViewLoadingCallback implements LoadingCallback {
+		private final Context mCtx;
+		private final WeakReference<ImageView> mImageViewReference;
+
+		public ImageViewLoadingCallback(Context ctx, ImageView imageView) {
+			this.mCtx = ctx;
+			this.mImageViewReference = new WeakReference<ImageView>(imageView);
+		}
+
+		@Override
+		public void setImageBitmap(Bitmap bitmap, boolean success) {
+			ImageView imageView = mImageViewReference.get();
+			if (imageView != null) {
+				imageView.setImageBitmap(bitmap);
 			}
 		}
-		return null;
+
+		@Override
+		public BitmapWorkerTask getBitmapWorkerTask() {
+			ImageView imageView = mImageViewReference.get();
+			if (imageView != null) {
+				final Drawable drawable = imageView.getDrawable();
+				if (drawable instanceof AsyncDrawable) {
+					final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+					return asyncDrawable.getBitmapWorkerTask();
+				}
+			}
+			return null;
+		}
+
+		@Override
+		public void setBitmapWorkerTask(BitmapWorkerTask task) {
+			ImageView imageView = mImageViewReference.get();
+			if (imageView != null) {
+				final AsyncDrawable asyncDrawable = new AsyncDrawable(mCtx,
+						task);
+				imageView.setImageDrawable(asyncDrawable);
+				asyncDrawable.start();
+			}
+		}
 	}
 
 	private static class AsyncDrawable extends AnimationDrawable {
@@ -135,16 +196,10 @@ public class BitmapWorkerTask extends AsyncTask<URL, Void, Bitmap> {
 			super();
 			Resources resources = ctx.getResources();
 			// TODO: here we need some nicer images
-			addFrame(
-					resources
-							.getDrawable(android.R.drawable.star_off),
-					500);
-			addFrame(
-					resources
-							.getDrawable(android.R.drawable.star_on),
-					500);
+			addFrame(resources.getDrawable(android.R.drawable.star_off), 500);
+			addFrame(resources.getDrawable(android.R.drawable.star_on), 500);
 			setOneShot(false);
-			
+
 			mBitmapWorkerTaskReference = new WeakReference<BitmapWorkerTask>(
 					bitmapWorkerTask);
 		}
--- Gallerista/src/de/marcusschiesser/gallerista/ui/ImageViewActivity.java
+++ Gallerista/src/de/marcusschiesser/gallerista/ui/ImageViewActivity.java
@@ -1,11 +1,19 @@
 package de.marcusschiesser.gallerista.ui;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import de.marcusschiesser.gallerista.R;
 import de.marcusschiesser.gallerista.tasks.BitmapWorkerTask;
+import de.marcusschiesser.gallerista.utils.ExceptionUtils;
 import de.marcusschiesser.gallerista.vo.ImageVO;
 
 /**
@@ -15,16 +23,56 @@ import de.marcusschiesser.gallerista.vo.ImageVO;
  */
 public class ImageViewActivity extends Activity {
 	public static final String EXTRA_SELECTED_IMAGE = "SELECTED_IMAGE";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.image_view);
-		Bundle extras = getIntent().getExtras();
-		ImageVO image = (ImageVO) extras.getSerializable(EXTRA_SELECTED_IMAGE);
-		ImageView imageView = (ImageView) findViewById(R.id.imageView);
-		BitmapWorkerTask.loadBitmap(this, image.getURL(), imageView);
+		final Bundle extras = getIntent().getExtras();
+		final ImageVO image = (ImageVO) extras
+				.getSerializable(EXTRA_SELECTED_IMAGE);
+		final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+		final Button wallpaperButton = (Button) findViewById(R.id.setWallpaperButton);
+		wallpaperButton.setVisibility(View.GONE);
+		BitmapWorkerTask.loadBitmap(this, image.getURL(), 
+				new BitmapWorkerTask.ImageViewLoadingCallback(this, imageView) {
+					@Override
+					public void setImageBitmap(Bitmap bitmap, boolean success) {
+						imageView.setImageBitmap(bitmap);
+						if (success)
+							wallpaperButton.setVisibility(View.VISIBLE);
+					}
+				});
+		wallpaperButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				BitmapWorkerTask.loadBitmap(ImageViewActivity.this,
+						image.getURL(),
+						new BitmapWorkerTask.DefaultLoadingCallback() {
+							@Override
+							public void setImageBitmap(Bitmap bitmap,
+									boolean success) {
+								if (success) {
+									WallpaperManager myWallpaperManager = WallpaperManager
+											.getInstance(getApplicationContext());
+									try {
+										myWallpaperManager.setBitmap(bitmap);
+										Toast.makeText(
+												getApplicationContext(),
+												R.string.image_view_wallpaper_changed,
+												Toast.LENGTH_LONG).show();
+									} catch (IOException e) {
+										ExceptionUtils.handleException(e,
+												R.string.error_wallpaper);
+									}
+								}
+							}
+						});
+
+			}
+		});
 	}
+
 }
